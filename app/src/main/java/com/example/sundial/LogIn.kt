bdf42d09d5f1a483c77e3461cee8f3ebae471b43
Dillon-Duncan <mrduncan.business@gmail.com>
2024-05-01 20:27:12 +0200

    Completed Firebase Authentication

--- app/src/main/java/com/example/sundial/LogIn.kt
+++ app/src/main/java/com/example/sundial/LogIn.kt
@@ -1,16 +1,50 @@
 package com.example.sundial
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class LogIn : AppCompatActivity() {
+
+    private lateinit var firebaseAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_log_in)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val buttonClick = findViewById<Button>(R.id.btnProceed)
+        buttonClick.setOnClickListener {
+            val email = findViewById<EditText>(R.id.edtEmail).text.toString()
+            val password = findViewById<EditText>(R.id.edtPassword).text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+
+                    firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            val intent = Intent(this, Dashboard::class.java)
+                            startActivity(intent)
+                        } else
+                        {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+            } else
+            {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
