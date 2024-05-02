686f1bef4aaf55240fb14d7262baef100500f066
ST10085622 <st10085622@vcconnect.edu.za>
2024-05-02 03:26:25 +0200

    updated menu & dashboard. also added new empty views for; timesheets and categories

---
+++ app/src/main/java/com/example/sundial/CreateTimeSheetEntry.kt
@@ -0,0 +1,20 @@
+package com.example.sundial
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CreateTimeSheetEntry : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_create_time_sheet_entry)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
