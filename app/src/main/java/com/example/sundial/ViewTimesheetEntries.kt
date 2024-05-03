21b2b0f9342d113d36935ca9f24c357f8cefff63
ST10085622 <st10085622@vcconnect.edu.za>
2024-05-03 23:44:01 +0200

    inserted menu on all screens; except settings. changed launcher icon to logo changed screen root layout to drawerlayout

--- app/src/main/java/com/example/sundial/ViewTimesheetEntries.kt
+++ app/src/main/java/com/example/sundial/ViewTimesheetEntries.kt
@@ -1,17 +1,24 @@
 package com.example.sundial
 
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -20,6 +27,9 @@ import com.google.firebase.database.ValueEventListener
 
 class ViewTimesheetEntries : AppCompatActivity() {
 
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drwLayout: DrawerLayout
+    private lateinit var navView: NavigationView
     private lateinit var category : String
     private lateinit var userRecyclerView: RecyclerView
     private lateinit var database: DatabaseReference
@@ -44,12 +54,57 @@ class ViewTimesheetEntries : AppCompatActivity() {
 
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.DrwLayoutViewTimeSheet)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val toolbar : Toolbar = findViewById(R.id.toolBarViewTimeSheet)
+        setSupportActionBar(toolbar)
+
+        drwLayout = findViewById(R.id.DrwLayoutViewTimeSheet)
+        toggle = ActionBarDrawerToggle(this, drwLayout, toolbar, R.string.open, R.string.close)
+        drwLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView = findViewById(R.id.nav_view_viewTimeSheet)
+
+        navView.setNavigationItemSelectedListener {
+            drwLayout.closeDrawer(GravityCompat.START)
+            when(it.itemId) {
+                R.id.itmAccount -> {intent = Intent(this, AccountDetails::class.java)
+                    startActivity(intent) }
+                R.id.itmAddCategory -> {intent = Intent(this, AddCategory::class.java)
+                    startActivity(intent) }
+                R.id.itmAddTimeSheet -> {intent = Intent(this, AddTimesheet::class.java)
+                    startActivity(intent) }
+                R.id.itmDashboard -> {intent = Intent(this, Dashboard::class.java)
+                    startActivity(intent) }
+                R.id.itmViewTimeSheetEntries -> {intent = Intent(this, ViewTimesheetEntries::class.java)
+                    startActivity(intent) }
+                R.id.itmSettings -> Toast.makeText(applicationContext, "Open Settings Layout", Toast.LENGTH_SHORT).show()
+                R.id.itmLogout -> { FirebaseAuth.getInstance().signOut()
+                    intent = Intent(this, MainActivity::class.java)
+                    finish()}
+
+            }
+            true
+        }
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (toggle.onOptionsItemSelected(item)) {
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
 
     private fun getUserData() {
 
